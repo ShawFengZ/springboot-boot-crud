a7017fd99d01234612cc98de3e3f332457e0d9ec
rainInteger <975981869@qq.com>
2018-08-02 14:42:53 +0800

    Internationalization

---
+++ src/main/java/com/atguigu/component/MyLocaleResolver.java
@@ -0,0 +1,34 @@
+package com.atguigu.component;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.LocaleResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+/**
+ * @author zxf
+ * @date 2018/8/2 14:22
+ */
+public class MyLocaleResolver implements LocaleResolver {
+
+    /**
+     * 解析区域信息
+     * */
+    @Override
+    public Locale resolveLocale(HttpServletRequest request) {
+        String l = request.getParameter("l");
+        Locale locale = Locale.getDefault();
+        if (!StringUtils.isEmpty(l)){
+            String[] split = l.split("_");
+            locale = new Locale(split[0], split[1]);
+        }
+        return locale;
+    }
+
+    @Override
+    public void setLocale(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Locale locale) {
+
+    }
+}
--- src/main/java/com/atguigu/config/MyMvcConfig.java
+++ src/main/java/com/atguigu/config/MyMvcConfig.java
@@ -1,39 +1,42 @@
-/*
 package com.atguigu.config;
 
+import com.atguigu.component.MyLocaleResolver;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-
-*/
-/**
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+/*
  * @author zxf
  * @date 2018/8/1 21:38
- *//*
-
+ */
 @Configuration
-public class MyMvcConfig extends WebMvcConfigurationSupport {
+public class MyMvcConfig extends WebMvcConfigurerAdapter {
 
-    @Override
+    /*@Override
     protected void addViewControllers(ViewControllerRegistry registry) {
         //super.addViewControllers(registry);
         //浏览器发送/atguigu 请求会来到success
         registry.addViewController("/atguigu").setViewName("success");
         registry.addViewController("/").setViewName("login");
-    }
+    }*/
 
     //所有的WebMvcConfigurerAdapter组件都会一起起作用
     @Bean//将组件注册在容器中
-    public WebMvcConfigurationSupport MyMvcConfig() {
-        WebMvcConfigurationSupport support = new WebMvcConfigurationSupport(){
+    public WebMvcConfigurerAdapter MyMvcConfig() {
+        WebMvcConfigurerAdapter adapter = new WebMvcConfigurerAdapter() {
             @Override
             public void addViewControllers(ViewControllerRegistry registry) {
-                registry.addViewController("/").setViewName("login");
-                registry.addViewController("/index.html").setViewName("login");
+                registry.addViewController("/").setViewName("index");
+                registry.addViewController("/index.html").setViewName("index");
             }
         };
-        return support;
+        return adapter;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
     }
 }
-*/
